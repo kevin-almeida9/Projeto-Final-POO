0bc24ebbdf762ee2506c943b67ed0325e313bc87
kevin-almeida9 <42482698+kevin-almeida9@users.noreply.github.com>
2018-12-05 21:04:39 -0200

    Revert "05/10 -18:19"
    This reverts commit f1069ed20bf56b9771c8b74d0e923367b7199d5b.

---
+++ 171213_171178/src/java/servico/CategoriaService.java
@@ -0,0 +1,38 @@
+
+package Servico;
+
+import Modelo.Categoria;
+import java.util.ArrayList;
+
+
+
+public class CategoriaService {
+    private ArrayList <Categoria>  listCategoria = Dados.getCategoriass();
+    
+    public void salvar (Categoria c){
+        listCategoria.add(c);
+    }
+    
+    public ArrayList<Categoria> getCategorias(){
+        return listCategoria;
+    }
+
+    public void setListCategoria(ArrayList<Categoria> listCategoria) {
+        this.listCategoria = listCategoria;
+    }
+    
+    public void remover(Categoria c){
+        listCategoria.remove(c);
+    }
+    
+     public Categoria getCategoriaByDescricao(String value) {
+    
+        for(Categoria e: listCategoria){
+           if(e.getDescricao().equals(value))
+               return e;
+        
+        }
+        
+        return null;
+    }
+}
